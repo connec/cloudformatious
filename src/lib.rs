a51269958ad3227eeef20beb40de4d3c203831f0
Chris Connelly <chris@connec.co.uk>
2021-04-16 22:40:50 +0100

    feat: Add a struct representing a stack event
    The `StackEvent` type presents all the information from the
    `rusoto_cloudformation::StackEvent` type, but with more accurate
    constraints and richer types. The main difference is that the top-level
    representation is as an enum with `Stack` and `Resource` variants. This
    is necessary because we have different types for `StackStatus` and
    `ResourceStatus`, which itself reflects CloudFormation's own model.
    
    To make using the enum easier, all the properties from
    `StackEventDetails` have accessors on the `StackEvent` enum. There's
    additionally an accessor for the `resource_status` which returns a
    `Status` trait object. Finally there's an `is_terminal` method that
    returns true for stack events with a terminal status, indicating that
    the relevant stack operation has finished.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #![warn(clippy::pedantic)]
 
-pub mod raw;
+mod event;
 mod status;
 
+pub mod raw;
+
+pub use event::{StackEvent, StackEventDetails};
 pub use status::{
     ChangeSetStatus, InvalidStatus, ResourceStatus, StackStatus, Status, StatusSentiment,
 };
