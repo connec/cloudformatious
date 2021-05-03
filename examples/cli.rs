848368952d9182dfde2a8841b462722ee128c73d
Chris Connelly <chris@connec.co.uk>
2021-05-03 14:52:25 +0100

    doc: Update cli example to include change set confirmation
    This now demonstrates a use-case of the `ApplyStack::change_set` API –
    previewing change sets before committing to their execution.

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,4 +1,4 @@
-use std::{convert::TryInto, env, process};
+use std::{convert::TryInto, env, io, process};
 
 use cloudformatious::{ApplyStackInput, CloudFormatious, DeleteStackInput, TemplateSource};
 use futures_util::StreamExt;
@@ -6,22 +6,6 @@ use rusoto_cloudformation::CloudFormationClient;
 use rusoto_core::Region;
 
 const USAGE: &str = "Usage: cargo run --example cli -- <apply|delete> <stack_name> [template_body]";
-
-enum Op {
-    Apply,
-    Delete,
-}
-
-impl std::str::FromStr for Op {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "apply" => Ok(Self::Apply),
-            "delete" => Ok(Self::Delete),
-            _ => Err(()),
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -38,28 +22,36 @@ async fn try_main() -> Result<(), Box<dyn std::error::Error>> {
         .collect::<Vec<_>>()
         .try_into()
         .map_err(|_| USAGE)?;
-    let op = op.parse().map_err(|()| USAGE)?;
 
     let client = CloudFormationClient::new(Region::default());
 
-    match op {
-        Op::Apply => {
+    match op.as_str() {
+        "apply" => {
             let template_body = env::args().nth(3).ok_or(USAGE)?;
 
             let input = ApplyStackInput::new(stack_name, TemplateSource::inline(template_body));
             let mut apply = client.apply_stack(input);
 
+            let change_set = apply.change_set().await?;
+            eprintln!("=== Change set ===");
+            eprintln!("{:#?}", change_set);
+            eprintln!();
+
+            confirm("Continue [Y/n]?").await?;
+            eprintln!();
+
             let mut events = apply.events();
             while let Some(event) = events.next().await {
                 eprintln!("{:?}", event);
             }
+            eprintln!();
 
             let output = apply.await?;
             eprintln!("=== Operation succeeded ===");
             eprintln!("{:#?}", output);
             eprintln!();
         }
-        Op::Delete => {
+        "delete" => {
             if env::args().nth(3).is_some() {
                 return Err(USAGE.into());
             }
@@ -71,11 +63,28 @@ async fn try_main() -> Result<(), Box<dyn std::error::Error>> {
             while let Some(event) = events.next().await {
                 eprintln!("{:?}", event);
             }
+            eprintln!();
 
             delete.await?;
             eprintln!("=== Operation succeeded ===");
         }
+        _ => return Err(USAGE.into()),
     }
 
     Ok(())
+}
+
+async fn confirm(prompt: &str) -> io::Result<()> {
+    eprint!("{} ", prompt);
+
+    tokio::task::spawn_blocking(|| {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        match input.as_str().trim() {
+            "Y" | "y" | "" => Ok(()),
+            _ => Err(io::Error::new(io::ErrorKind::Other, "Quitting")),
+        }
+    })
+    .await
+    .unwrap()
 }
