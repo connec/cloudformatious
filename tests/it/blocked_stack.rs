43a13de0b93435e353ea55561e2219ef7f6fbc1f
Chris Connelly <chris@connec.co.uk>
2022-12-02 00:33:28 +0000

    feat!: add `ApplyStackError::Blocked` variant
    Attempting to perform an `apply_stack` operation on a stack in a blocked
    status will now cause an `ApplyStackError::Blocked { .. }` error to be
    returned. This offers direct access to the `BlockedStackStatus`, making
    it easier to implement appropriate handling, if desired.
    
    BREAKING CHANGE: A `Blocked` variant has been added to the
    `ApplyStackError` enum, which is exhaustive.

--- tests/it/blocked_stack.rs
+++ tests/it/blocked_stack.rs
@@ -1,8 +1,9 @@
 use std::convert::TryFrom;
 
-use cloudformatious::BlockedStackStatus;
+use assert_matches::assert_matches;
+use cloudformatious::{ApplyStackError, ApplyStackInput, BlockedStackStatus, Client, Parameter};
 
-use crate::common::{clean_up, get_client, stack_with_status};
+use crate::common::{clean_up, get_client, stack_with_status, NON_EMPTY_TEMPLATE};
 
 #[tokio::test]
 async fn create_failed() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,6 +12,14 @@ async fn create_failed() -> Result<(), Box<dyn std::error::Error>> {
 
     let status = BlockedStackStatus::try_from(failure.stack_status).unwrap();
     assert_eq!(status, BlockedStackStatus::CreateFailed);
+
+    let error = try_update(&client, &failure.stack_id).await;
+    assert_matches!(
+        error,
+        ApplyStackError::Blocked {
+            status: BlockedStackStatus::CreateFailed,
+        }
+    );
 
     clean_up(failure.stack_id).await?;
 
@@ -25,6 +34,14 @@ async fn rollback_failed() -> Result<(), Box<dyn std::error::Error>> {
     let status = BlockedStackStatus::try_from(failure.stack_status).unwrap();
     assert_eq!(status, BlockedStackStatus::RollbackFailed);
 
+    let error = try_update(&client, &failure.stack_id).await;
+    assert_matches!(
+        error,
+        ApplyStackError::Blocked {
+            status: BlockedStackStatus::RollbackFailed,
+        }
+    );
+
     clean_up(failure.stack_id).await?;
 
     Ok(())
@@ -37,6 +54,14 @@ async fn delete_failed() -> Result<(), Box<dyn std::error::Error>> {
 
     let status = BlockedStackStatus::try_from(failure.stack_status).unwrap();
     assert_eq!(status, BlockedStackStatus::DeleteFailed);
+
+    let error = try_update(&client, &failure.stack_id).await;
+    assert_matches!(
+        error,
+        ApplyStackError::Blocked {
+            status: BlockedStackStatus::DeleteFailed,
+        }
+    );
 
     clean_up(failure.stack_id).await?;
 
@@ -51,6 +76,14 @@ async fn update_failed() -> Result<(), Box<dyn std::error::Error>> {
     let status = BlockedStackStatus::try_from(failure.stack_status).unwrap();
     assert_eq!(status, BlockedStackStatus::UpdateFailed);
 
+    let error = try_update(&client, &failure.stack_id).await;
+    assert_matches!(
+        error,
+        ApplyStackError::Blocked {
+            status: BlockedStackStatus::UpdateFailed,
+        }
+    );
+
     clean_up(failure.stack_id).await?;
 
     Ok(())
@@ -64,7 +97,31 @@ async fn update_rollback_failed() -> Result<(), Box<dyn std::error::Error>> {
     let status = BlockedStackStatus::try_from(failure.stack_status).unwrap();
     assert_eq!(status, BlockedStackStatus::UpdateRollbackFailed);
 
+    let error = try_update(&client, &failure.stack_id).await;
+    assert_matches!(
+        error,
+        ApplyStackError::Blocked {
+            status: BlockedStackStatus::UpdateRollbackFailed,
+        }
+    );
+
     clean_up(failure.stack_id).await?;
 
     Ok(())
+}
+
+async fn try_update(client: &Client, stack_name: &str) -> ApplyStackError {
+    client
+        .apply_stack(
+            ApplyStackInput::new(
+                stack_name,
+                cloudformatious::TemplateSource::inline(NON_EMPTY_TEMPLATE),
+            )
+            .set_parameters([Parameter {
+                key: "CidrBlock".to_string(),
+                value: "10.0.0.0/28".to_string(),
+            }]),
+        )
+        .await
+        .unwrap_err()
 }
