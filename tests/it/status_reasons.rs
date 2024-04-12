f9468576b128ec26680a8378987d522babec9291
Chris Connelly <chris@connec.co.uk>
2024-04-12 13:44:13 +0100

    chore: remove unreproducible test cases
    This may obviate some of the status reason analysis, but we won't change
    the source until we upgrade the AWS SDK.

--- tests/it/status_reasons.rs
+++ tests/it/status_reasons.rs
@@ -6,43 +6,8 @@ use cloudformatious::{
 };
 
 use crate::common::{
-    clean_up, generated_name, get_client, get_sdk_config, AUTHORIZATION_FAILURE_TEMPLATE,
-    MISSING_PERMISSION_1_TEMPLATE, MISSING_PERMISSION_2_TEMPLATE,
+    clean_up, generated_name, get_client, get_sdk_config, MISSING_PERMISSION_TEMPLATE,
 };
-
-#[tokio::test]
-async fn status_reason_missing_permission_no_principal() -> Result<(), Box<dyn std::error::Error>> {
-    let client = get_client().await;
-
-    let stack_name = generated_name();
-    let input = ApplyStackInput::new(
-        &stack_name,
-        TemplateSource::inline(MISSING_PERMISSION_1_TEMPLATE),
-    );
-    let error = client.apply_stack(input).await.unwrap_err();
-
-    let failure = assert_matches!(error, ApplyStackError::Failure(failure) => failure);
-    assert_eq!(failure.stack_status, StackStatus::RollbackComplete);
-
-    let status_reason = assert_matches!(
-        &failure.resource_events[..],
-        [(ResourceStatus::CreateFailed, status)] if status.logical_resource_id() == "Bucket" => {
-            status.resource_status_reason()
-        }
-    );
-    let missing_permission = assert_matches!(
-      status_reason.detail(),
-      Some(StatusReasonDetail::MissingPermission(missing_permission)) => missing_permission
-    );
-
-    assert_eq!(missing_permission.permission, "s3:CreateBucket");
-    assert_eq!(missing_permission.principal, None);
-    assert!(missing_permission.encoded_authorization_message.is_none());
-
-    clean_up(stack_name).await?;
-
-    Ok(())
-}
 
 #[tokio::test]
 async fn status_reason_missing_permission_with_principal() -> Result<(), Box<dyn std::error::Error>>
@@ -56,7 +21,7 @@ async fn status_reason_missing_permission_with_principal() -> Result<(), Box<dyn
     let stack_name = generated_name();
     let input = ApplyStackInput::new(
         &stack_name,
-        TemplateSource::inline(MISSING_PERMISSION_2_TEMPLATE),
+        TemplateSource::inline(MISSING_PERMISSION_TEMPLATE),
     );
     let error = client.apply_stack(input).await.unwrap_err();
 
@@ -80,40 +45,6 @@ async fn status_reason_missing_permission_with_principal() -> Result<(), Box<dyn
     );
     assert_eq!(missing_permission.principal, identity.arn.as_deref());
     assert!(missing_permission.encoded_authorization_message.is_none());
-
-    clean_up(stack_name).await?;
-
-    Ok(())
-}
-
-#[tokio::test]
-async fn status_reason_authorization_failure() -> Result<(), Box<dyn std::error::Error>> {
-    let client = get_client().await;
-
-    let stack_name = generated_name();
-    let input = ApplyStackInput::new(
-        &stack_name,
-        TemplateSource::inline(AUTHORIZATION_FAILURE_TEMPLATE),
-    );
-    let error = client.apply_stack(input).await.unwrap_err();
-
-    let failure = assert_matches!(error, ApplyStackError::Failure(failure) => failure);
-    assert_eq!(failure.stack_status, StackStatus::RollbackComplete);
-
-    let status_reason = assert_matches!(
-        &failure.resource_events[..],
-        [(ResourceStatus::CreateFailed, status)] if status.logical_resource_id() == "Vpc" => {
-            status.resource_status_reason()
-        }
-    );
-    let encoded_message = assert_matches!(
-      status_reason.detail(),
-      Some(StatusReasonDetail::AuthorizationFailure(m)) => m
-    );
-
-    let sdk_config = get_sdk_config().await;
-    let decoded_message = encoded_message.decode(&sdk_config).await?;
-    assert_eq!(decoded_message["context"]["action"], "ec2:CreateVpc");
 
     clean_up(stack_name).await?;
 
